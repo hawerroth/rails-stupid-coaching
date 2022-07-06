791b4e7969122f4b5dde6768b19f5991f1f1ca39
Angela Amaral <angela.hawerroth@gmail.com>
2022-07-06 11:29:48 +0900

    Challenge Stupid-Coaching

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #verb "url-caminho to controller_name#action_name"
+  get '/ask', to: 'questions#ask'
 end
