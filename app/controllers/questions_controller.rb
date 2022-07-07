47c099a19622c8a601a640655ef03d8bfe696ec7
Angela Amaral <angela.hawerroth@gmail.com>
2022-07-08 08:48:06 +0900

    Rails Stupid Coaching

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,3 +1,20 @@
 class QuestionsController < ApplicationController
   def ask; end
+
+  def answer
+    @question = params[:question]
+    @answer = answer_coach
+  end
+end
+
+private
+
+def answer_coach
+  if @question.include? '?'
+    'Silly question, get dressed and go to work!'
+  elsif @question.end_with? 'I am going to work'
+    'Great!'
+  else
+    'I don\'t care, get dressed and go to work!'
+  end
 end
